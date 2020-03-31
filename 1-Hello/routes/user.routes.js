240555fe3e09c638b83db4011601f9ea0584d06a
DSVences <DucSoanVences99@gmail.com>
2020-03-31 21:52:11 +0700

    Add controller (MVC)

--- 1-Hello/routes/user.routes.js
+++ 1-Hello/routes/user.routes.js
@@ -1,43 +1,16 @@
 var express = require('express');
 var router = express.Router();
-var shortid = require('shortid');
-var db = require('../db');
 
-router.get('/', function(req, res){
-	res.render('users/index', {
-		users: db.get('users').value()
-	});
-});
+var controller = require('../controllers/user.controller');
 
-router.get('/search', function(req, res){
-	var q = req.query.q;
-	var matchedUsers = users.filter(function(user) {
-		return user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1;
-	});
+router.get('/', controller.index);
 
-	res.render('users/index', {
-		users: matchedUsers
-	});
-});
+router.get('/search', controller.search);
 
-router.get('/create', function(req, res){
-	res.render('users/create');
-});
+router.get('/create', controller.create);
 
-router.get('/:id', function(req, res){
-	var id = req.params.id;
+router.get('/:id', controller.get);
 
-	var user = db.get('users').find({id: id}).value();
-
-	res.render('users/view', {
-		user: user
-	});
-});
-
-router.post('/create', function(req, res){
-	req.body.id = shortid.generate();
-	db.get('users').push(req.body).write();
-	res.redirect('/users');
-});
+router.post('/create', controller.postCreate);
 
 module.exports = router;
