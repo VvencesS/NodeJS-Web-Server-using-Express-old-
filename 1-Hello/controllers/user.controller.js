57feaa0118efa9fdd459d75ddbcf64ba0d0f1cb0
DSVences <DucSoanVences99@gmail.com>
2020-04-02 22:02:05 +0700

    Server-side validation - create vadilation for create form

--- 1-Hello/controllers/user.controller.js
+++ 1-Hello/controllers/user.controller.js
@@ -1,4 +1,5 @@
 var db = require('../db');
+var shortid = require('short-id')
 
 module.exports.index = function(req, res){
 	res.render('users/index', {
@@ -33,6 +34,23 @@ module.exports.get = function(req, res){
 
 module.exports.postCreate = function(req, res){
 	req.body.id = shortid.generate();
+	var errors = [];
+
+	if(!req.body.name){
+		errors.push('Name is required');
+	}
+
+	if(!req.body.phone){
+		errors.push('Phone is required');
+	}
+
+	if(errors.length){
+		res.render('users/create', {
+			errors: errors,
+			values: req.body
+		});
+		return;
+	}
 	db.get('users').push(req.body).write();
 	res.redirect('/users');
 };
--- 1-Hello/index.js
+++ 1-Hello/index.js
@@ -14,6 +14,8 @@ app.set('views', './views');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+app.use(express.static('public'));
+
 app.get('/', function(req, res){
 	res.render('index', {
 		name: 'AAA'
