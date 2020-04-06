f152c5c76199cb8fe97bdc575cd1ab5244423e31
DSVences <DucSoanVences99@gmail.com>
2020-04-06 22:52:31 +0700

    Cookie and install npm cookie-parse

--- 1-Hello/index.js
+++ 1-Hello/index.js
@@ -3,17 +3,17 @@ var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
 
 var userRoute = require('./routes/user.routes');
-
 var db = require('./db');
 
 var port = 3000;
 
 var app = express();
 app.set('view engine', 'pug');
-app.set('views', './views');	
+app.set('views', './views');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser());
 
 app.use(express.static('public'));
 
