ecfd244d18c1c1a115b327e045562bc444388b28
DSVences <DucSoanVences99@gmail.com>
2020-04-07 23:22:57 +0700

    Authentication (Login)

--- 1-Hello/routes/user.routes.js
+++ 1-Hello/routes/user.routes.js
@@ -2,9 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 var controller = require('../controllers/user.controller');
-var validate = require('../Validate/user.validate')
+var validate = require('../Validate/user.validate');
+var authMiddleware = require('../middlewares/auth.middleware');
 
-router.get('/', controller.index);
+router.get('/', authMiddleware.requireAuth, controller.index);
 
 router.get('/cookie', function(req, res, next){
 	res.cookie('user-id', 12345);
