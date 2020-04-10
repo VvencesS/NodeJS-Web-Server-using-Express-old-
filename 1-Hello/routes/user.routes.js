31d0cbee9afb48d0ef54c9c6e8468130498034c3
DSVences <DucSoanVences99@gmail.com>
2020-04-10 11:42:20 +0700

    File upload - install npm multer and upload imgage

--- 1-Hello/routes/user.routes.js
+++ 1-Hello/routes/user.routes.js
@@ -1,4 +1,7 @@
 var express = require('express');
+var multer  = require('multer');
+
+var upload = multer({ dest: './public/uploads/' });
 var router = express.Router();
 
 var controller = require('../controllers/user.controller');
@@ -17,6 +20,10 @@ router.get('/create', controller.create);
 
 router.get('/:id', controller.get);
 
-router.post('/create', validate.postCreate, controller.postCreate);
+router.post('/create', 
+	upload.single('avatar'), 
+	validate.postCreate, 
+	controller.postCreate
+);
 
 module.exports = router;
